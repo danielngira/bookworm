068ddfa4f8f16b3cb19745768762d2cb01668381
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2025-03-09 01:47:35 -0600

    removed secrets

---  firebase-config.tsx
+++  firebase-config.tsx
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDit0JiCTpshFrITMqWwGnFAcgp5x17tTY",
-  authDomain: "bookworm-19685.firebaseapp.com",
-  projectId: "bookworm-19685",
-  storageBucket: "bookworm-19685.firebasestorage.app",
-  messagingSenderId: "910522458582",
-  appId: "1:910522458582:web:494fa0aef68cdb898b8477",
-  measurementId: "G-NRB8XBQZVF",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
