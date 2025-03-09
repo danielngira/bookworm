b0cb78c1c85c7e4709f5e28f567565bc7682706a
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2025-03-09 03:42:05 -0500

    implemented sign up

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Profile from "./pages/profile";
 import Home from "./pages/home";
 import MyBooks from "./pages/mybooks";
 import Settings from "./pages/settings";
+import Signup from "./pages/sign-up";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/bookworm/signup" element={<Signup />} />
       <Route path="/bookworm/mybooks" element={<MyBooks />} />
       <Route path="/bookworm/profile" element={<Profile />}>
         <Route path="settings" element={<Settings />} />
